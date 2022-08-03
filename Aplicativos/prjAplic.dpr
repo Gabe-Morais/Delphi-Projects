64ccb6989e0299a9643b09899724b12cec8d8070
Gabriel Morais <gabe-morais@outlook.com>
2022-08-02 22:15:50 -0300

    START

---
+++ Aplicativos/prjAplic.dpr
@@ -0,0 +1,17 @@
+program prjAplic;
+
+uses
+  Vcl.Forms,
+  untAplic in 'untAplic.pas' {Form1},
+  Vcl.Themes,
+  Vcl.Styles;
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  TStyleManager.TrySetStyle('Tablet Dark');
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
