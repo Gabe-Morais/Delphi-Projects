c2401feeb853e68b8d13d7314f95aea603fdf754
Gabriel Morais <gabe-morais@outlook.com>
2022-08-25 22:13:00 -0300

    wip

---
+++ Login/__recovery/untPrincipal.dfm
@@ -0,0 +1,27 @@
+object frmPrincipal: TfrmPrincipal
+  Left = 0
+  Top = 0
+  Caption = 'Menu Principal'
+  ClientHeight = 624
+  ClientWidth = 901
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Image1: TImage
+    Left = 0
+    Top = 0
+    Width = 901
+    Height = 624
+    Align = alClient
+    ExplicitLeft = 320
+    ExplicitTop = 248
+    ExplicitWidth = 105
+    ExplicitHeight = 105
+  end
+end
---
+++ Login/untLogin.pas
@@ -0,0 +1,76 @@
+unit untLogin;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Imaging.pngimage,
+  Vcl.StdCtrls, Vcl.Buttons, FireDAC.Stan.Intf, FireDAC.Stan.Option,
+  FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
+  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Stan.Param,
+  FireDAC.DatS, FireDAC.DApt.Intf, FireDAC.DApt, FireDAC.VCLUI.Wait,
+  FireDAC.Phys.MySQLDef, FireDAC.Phys.MySQL, FireDAC.Comp.UI, Data.DB,
+  FireDAC.Comp.DataSet, FireDAC.Comp.Client;
+
+type
+  TfrmLogin = class(TForm)
+    Image1: TImage;
+    edtLogin: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    edtSenha: TEdit;
+    btnLogar: TBitBtn;
+    btnLimpar: TBitBtn;
+    conn: TFDConnection;
+    FDQuery1: TFDQuery;
+    FDGUIxWaitCursor1: TFDGUIxWaitCursor;
+    FDPhysMySQLDriverLink1: TFDPhysMySQLDriverLink;
+    FDQuery1LOGIN: TStringField;
+    FDQuery1SENHA: TStringField;
+    procedure btnLimparClick(Sender: TObject);
+    procedure btnLogarClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmLogin: TfrmLogin;
+
+implementation
+
+{$R *.dfm}
+
+uses untPrincipal;
+
+procedure TfrmLogin.btnLimparClick(Sender: TObject);
+begin
+  edtLogin.Clear;
+  edtSenha.clear;
+  edtLogin.SetFocus;
+end;
+
+procedure TfrmLogin.btnLogarClick(Sender: TObject);
+var vSQL: String;
+begin
+    vSQL:= '';
+    vSQL := ' SELECT * FROM LOGIN '+
+            ' WHERE LOGIN = ' + QuotedStr(edtLogin.Text) +
+            ' AND SENHA = ' + QuotedStr(edtSenha.Text);
+
+            FDQuery1.close;
+            FDQuery1.sql.CommaText := vSQL;
+            FDQuery1.Open;
+
+            if FDQuery1.IsEmpty then
+              Application.MessageBox('Usuário ou senha inválidos', 'Atenção'
+              , MB_ICONINFORMATION + MB_OK)
+            else
+            begin
+                frmPrincipal.show;
+                frmLogin.Hide;
+            end;
+end;
+
+end.
